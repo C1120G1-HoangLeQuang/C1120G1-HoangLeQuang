aff96c63df197569944f0bf639eff8880efcfc61
lequang123-lang <75114966+lequang123-lang@users.noreply.github.com>
2020-12-07 21:29:45 +0700

    update bai tap jse

---
+++ module1/thuc_hanh_js/Su_dung_toan_tu_js.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt();
+inputHeight = prompt();
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
