1a1050dff9e2f6e34385a55f9413a7861e82ffba
Mac <mac@Macs-Air.mshome.net>
2021-03-04 15:19:58 +0700

    update

--- module3/case_study/case_study.sql
+++ module3/case_study/case_study.sql
@@ -45,11 +45,11 @@ create table nhan_vien(
 id_nhanvien int not null auto_increment primary key,
 ho_ten varchar(45) not null,
 id_vitri int not null,
-foreign key(id_vitri) references vi_tri(id_vitri),
+foreign key(id_vitri) references vi_tri(id_vitri) on update cascade on delete cascade,
 id_trinhdo int not null,
-foreign key(id_trinhdo) references trinh_do(id_trinhdo),
+foreign key(id_trinhdo) references trinh_do(id_trinhdo) on update cascade on delete cascade,
 id_bophan int not null,
-foreign key(id_bophan) references bo_phan(id_bophan),
+foreign key(id_bophan) references bo_phan(id_bophan) on update cascade on delete cascade,
 ngay_sinh date not null,
 so_cmnd varchar(45) not null,
 luong varchar(45) not null,
@@ -61,7 +61,7 @@ dia_chi varchar(45) not null
 create table khach_hang(
 id_khachhang int not null auto_increment primary key,
 id_loaikhach int not null,
-foreign key(id_loaikhach) references loai_khach(id_loaikhach),
+foreign key(id_loaikhach) references loai_khach(id_loaikhach) on update cascade on delete cascade,
 ho_ten varchar(45) not null,
 ngay_sinh date not null,
 so_cmnd varchar(45) not null,
@@ -76,36 +76,39 @@ ten_dich_vu varchar(45) not null,
 dien_tich int not null,
 so_tang int not null,
 songuoi_toida int not null,
-chi_phi_thue varchar(45) not null,
+chi_phi_thue int not null,
 id_kieuthue int not null,
-foreign key(id_kieuthue) references kieu_thue(id_kieuthue),
+foreign key(id_kieuthue) references kieu_thue(id_kieuthue) on update cascade on delete cascade,
 id_loaidichvu int not null,
-foreign key(id_loaidichvu) references loai_dich_vu(id_loaidichvu),
+foreign key(id_loaidichvu) references loai_dich_vu(id_loaidichvu) on update cascade on delete cascade,
 trang_thai varchar(45) not null
 );
+
 
 create table hop_dong(
 id_hopdong int not null auto_increment primary key,
 id_nhanvien int not null,
-foreign key(id_nhanvien) references nhan_vien(id_nhanvien),
+foreign key(id_nhanvien) references nhan_vien(id_nhanvien) on update cascade on delete cascade,
 id_khachhang int not null,
-foreign key(id_khachhang) references khach_hang(id_khachhang),
+foreign key(id_khachhang) references khach_hang(id_khachhang) on update cascade on delete cascade,
 id_dichvu int not null,
-foreign key(id_dichvu) references dich_vu(id_dichvu),
+foreign key(id_dichvu) references dich_vu(id_dichvu) on update cascade on delete cascade,
 ngaylam_hopdong date not null,
 ngay_ketthuc date not null, 
 tien_datcoc int not null,
 tong_tien int not null
 );
 
+
 create table hop_dong_chi_tiet(
 id_hopdongchitiet int not null auto_increment primary key,
 id_hopdong int not null,
-foreign key(id_hopdong) references hop_dong(id_hopdong),
+foreign key(id_hopdong) references hop_dong(id_hopdong) on update cascade on delete cascade,
 id_dichvudikem int not null,
-foreign key(id_dichvudikem) references dich_vu_di_kem(id_dichvudikem),
+foreign key(id_dichvudikem) references dich_vu_di_kem(id_dichvudikem) on update cascade on delete cascade,
 so_luong int not null
 );
+
 
 
 
--- module3/case_study/task2_28.sql
+++ module3/case_study/task2_28.sql
@@ -93,7 +93,7 @@ select distinct ho_ten from khach_hang;
 /* Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2019 
 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng */ 
 
-select count(id_hopdong), hop_dong.id_khachhang, ngaylam_hopdong from hop_dong
+select count(id_hopdong), hop_dong.id_khachhang, month(ngaylam_hopdong) as thang from hop_dong
 inner join khach_hang on hop_dong.id_khachhang = khach_hang.id_khachhang
 where ngaylam_hopdong between '2019/01/01' AND '2019/12/31'
 group by id_hopdong;
@@ -124,6 +124,174 @@ where loai_khach.ten_loaikhach = "Gold" and khach_hang.dia_chi = "Quảng Trị"
 /* Hiển thị thông tin IDHopDong, TenNhanVien, TenKhachHang, SoDienThoaiKhachHang, TenDichVu, 
 SoLuongDichVuDikem (được tính dựa trên tổng Hợp đồng chi tiết), TienDatCoc 
 của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2019 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2019 */
+
+use Casestudy;
+select hop_dong.id_hopdong, nhan_vien.ho_ten, khach_hang.ho_ten, khach_hang.sdt, dich_vu.ten_dich_vu,
+count(hop_dong_chi_tiet.id_hopdongchitiet) as soluong_dichvudikem, tien_datcoc from hop_dong
+inner join nhan_vien on nhan_vien.id_nhanvien = hop_dong.id_nhanvien
+inner join khach_hang on khach_hang.id_khachhang = hop_dong.id_khachhang
+inner join dich_vu on dich_vu.id_dichvu = hop_dong.id_dichvu
+inner join hop_dong_chi_tiet on hop_dong_chi_tiet.id_hopdong = hop_dong.id_hopdong
+where hop_dong.ngaylam_hopdong between '2019/10/01' and '2019/12/31'
+group by hop_dong_chi_tiet.id_hopdongchitiet;
+
+/* Task 13 Case study */
+/* Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
+(Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau) */
+ 
+select dich_vu_di_kem.id_dichvudikem, ten_dichvudikem, gia, don_vi,
+trangthai_khadung, count(hop_dong_chi_tiet.so_luong) as soluong_dichvudikem from dich_vu_di_kem
+inner join hop_dong_chi_tiet on hop_dong_chi_tiet.id_dichvudikem = dich_vu_di_kem.id_dichvudikem
+group by hop_dong_chi_tiet.id_dichvudikem
+having max(hop_dong_chi_tiet.so_luong);
+
+/* Task 14 Case study */
+/* Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
+Thông tin hiển thị bao gồm IDHopDong, TenLoaiDichVu, TenDichVuDiKem, SoLanSuDung */
+
+select hop_dong.id_hopdong, loai_dich_vu.ten_loaidichvu, ten_dichvudikem, hop_dong_chi_tiet.so_luong, 
+dich_vu_di_kem.id_dichvudikem, ten_dichvudikem, gia, don_vi, trangthai_khadung from dich_vu_di_kem
+inner join hop_dong_chi_tiet on hop_dong_chi_tiet.id_dichvudikem = dich_vu_di_kem.id_dichvudikem
+inner join hop_dong on hop_dong.id_hopdong = hop_dong_chi_tiet.id_hopdong
+inner join dich_vu on dich_vu.id_dichvu = hop_dong.id_dichvu
+inner join loai_dich_vu on loai_dich_vu.id_loaidichvu = dich_vu.id_loaidichvu
+where hop_dong_chi_tiet.so_luong = 1;
+
+/* Task 15 Case study */
+/* Hiển thi thông tin của tất cả nhân viên bao gồm IDNhanVien, HoTen, TrinhDo, TenBoPhan, SoDienThoai, DiaChi 
+mới chỉ lập được tối đa 3 hợp đồng từ năm 2018 đến 2019 */
+
+select nhan_vien.id_nhanvien, nhan_vien.ho_ten, trinh_do.trinh_do, bo_phan.ten_bophan,
+nhan_vien.sdt, nhan_vien.dia_chi, count(hop_dong.id_nhanvien) as so_lan from nhan_vien
+inner join trinh_do on trinh_do.id_trinhdo = nhan_vien.id_trinhdo
+inner join bo_phan on bo_phan.id_bophan = nhan_vien.id_bophan
+inner join hop_dong on hop_dong.id_nhanvien = nhan_vien.id_nhanvien
+where (hop_dong.ngaylam_hopdong between '2018/01/01' and '2019/12/31')
+group by nhan_vien.id_nhanvien
+having count(hop_dong.id_nhanvien) <= 3;
+
+/* Task 16 Case study */
+/* Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2017 đến năm 2021 */
+
+delete from nhan_vien where id_nhanvien not in
+(
+	select hop_dong.id_nhanvien
+    from hop_dong
+    where year(ngaylam_hopdong) between 2017 and 2021
+);
+
+/* Task 17 Case study */
+/* Cập nhật thông tin những khách hàng có TenLoaiKhachHang từ  Gold lên Platinum, 
+chỉ cập nhật những khách hàng đã từng đặt phòng với tổng Tiền thanh toán trong năm 2019 là lớn hơn 200 USD */
+
+create or replace view task172 as
+	select 	khach_hang.id_loaikhach,
+			sum(dich_vu.chi_phi_thue + (hop_dong_chi_tiet.so_luong*dich_vu_di_kem.gia)) as total 
+    from hop_dong
+	inner join khach_hang on khach_hang.id_khachhang = hop_dong.id_khachhang
+	inner join loai_khach on khach_hang.id_loaikhach = loai_khach.id_loaikhach
+	inner join dich_vu on dich_vu.id_dichvu = hop_dong.id_dichvu
+	inner join hop_dong_chi_tiet on hop_dong_chi_tiet.id_hopdong = hop_dong.id_hopdong
+	inner join dich_vu_di_kem on dich_vu_di_kem.id_dichvudikem = hop_dong_chi_tiet.id_dichvudikem
+    where hop_dong.ngaylam_hopdong between '2019/01/01' and '2019/12/31'
+	group by hop_dong.id_khachhang
+	having sum((dich_vu.chi_phi_thue+(hop_dong_chi_tiet.so_luong*dich_vu_di_kem.gia))) > 200;
+
+UPDATE khach_hang
+set id_loaikhach = 2
+where (id_loaikhach = 3) 
+and (id_loaikhach in 
+	(
+		select id_loaikhach from task172
+    )
+);
+
+/* Task 18 Case study */
+/* Xóa những khách hàng có hợp đồng trước năm 2019 (chú ý ràngbuộc giữa các bảng) */
+
+delete from khach_hang
+where id_khachhang in
+(select hop_dong.id_khachhang from hop_dong
+where year(ngaylam_hopdong) < 2018);
+
+/* Task 19 Case study */
+/* Cập nhật giá cho các Dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2019 lên gấp đôi */
+
+create temporary table table19
+select dich_vu_di_kem.id_dichvudikem, ten_dichvudikem, gia , don_vi,
+trangthai_khadung, sum(hop_dong_chi_tiet.so_luong) as soluong_dichvudikem from dich_vu_di_kem
+inner join hop_dong_chi_tiet on hop_dong_chi_tiet.id_dichvudikem = dich_vu_di_kem.id_dichvudikem
+group by hop_dong_chi_tiet.id_dichvudikem
+having sum(hop_dong_chi_tiet.so_luong) > 10;
+
+update dich_vu_di_kem
+set gia = gia * 2
+where id_dichvudikem in 
+	(
+		select id_dichvudikem  from table19
+    )
+;
+
+/* Task 20 Case study */
+/* Hiển thị thông tin của tất cả các Nhân viên và Khách hàng có trong hệ thống, 
+thông tin hiển thị bao gồm ID (IDNhanVien, IDKhachHang), HoTen, Email, SoDienThoai, NgaySinh, DiaChi */
+
+select id_nhanvien as id, ho_ten, email, sdt, ngay_sinh, dia_chi from nhan_vien
+union all
+select id_khachhang as id, ho_ten, email, sdt, ngay_sinh, dia_chi from khach_hang;
+
+/* Task 21 Case study */
+/* Tạo khung nhìn có tên là V_NHANVIEN để lấy được thông tin của tất cả các nhân viên có địa chỉ 
+là “Quảng Nam” và đã từng lập hợp đồng cho 1 hoặc nhiều Khách hàng bất kỳ  với ngày lập hợp đồng là “12/01/2019” */
+
+create or replace view V_NHANVIEN as
+select nhan_vien.id_nhanvien, nhan_vien.ho_ten, nhan_vien.dia_chi, id_hopdong, ngaylam_hopdong from hop_dong
+inner join nhan_vien on nhan_vien.id_nhanvien = hop_dong.id_nhanvien
+where nhan_vien.dia_chi = "Quảng Nam" and hop_dong.ngaylam_hopdong = "2019/01/12";
+
+/* Task 22 Case study */
+/* Thông qua khung nhìn V_NHANVIEN thực hiện cập nhật địa chỉ thành “Liên Chiểu” 
+đối với tất cả các Nhân viên được nhìn thấy bởi khung nhìn này. */
+
+create or replace view V_NHANVIEN2 as
+	select nhan_vien.id_nhanvien as ID, nhan_vien.ho_ten, nhan_vien.dia_chi as dia_chi_update, 
+    hop_dong.id_hopdong, hop_dong.ngaylam_hopdong from nhan_vien
+	inner join hop_dong on nhan_vien.id_nhanvien = hop_dong.id_nhanvien
+	where nhan_vien.dia_chi = "Quảng Nam" and hop_dong.ngaylam_hopdong = "2019/01/12";
+
+update v_nhanvien2
+set dia_chi_update = "Liên Chiểu";
+-- where nhan_vien.id_nhanvien in (
+-- 	select ID from v_nhanvien2);
+
+/*select table_name, is_updatable
+from information_schema.views
+where table_schema = 'Casestudy';*/
+
+/* Task 23 Case study */
+/* Tạo Store procedure Sp_1 Dùng để xóa thông tin của một Khách hàng nào đó với Id Khách hàng 
+được truyền vào như là 1 tham số của Sp_1 */
+
+delimiter //
+create procedure deleteInforCustomerById(in idDelete int)
+begin 
+delete from khach_hang
+where khach_hang.id_khachhang = idDelete;
+end //
+delimiter ;
+
+call deleteInforCustomerById(5);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
