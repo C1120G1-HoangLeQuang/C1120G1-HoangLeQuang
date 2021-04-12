e25767f89c4a688b592c6935d4deb5cb6dded7b2
Mac <mac@Erwins-MacBook-Air.local>
2021-04-13 01:47:20 +0700

    update

--- Module4/session_11_Webservice/bai_tap/blog_RESTful/src/main/java/com/example/blog/controller/BlogController.java
+++ Module4/session_11_Webservice/bai_tap/blog_RESTful/src/main/java/com/example/blog/controller/BlogController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Optional;
@@ -52,6 +54,48 @@ public class BlogController {
         return new ResponseEntity<Blog>(blog, HttpStatus.OK);
     }
 
+    //-------------------Create a Blog--------------------------------------------------------
+
+    @PostMapping("/blog/create")
+    public ResponseEntity<Blog> createBlog(@RequestBody Blog blog, UriComponentsBuilder builder) {
+        this.blogService.save(blog);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(builder.path("/blog/create/{id}").buildAndExpand(blog.getId()).toUri());
+        return new ResponseEntity<Blog>(httpHeaders, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/blog/update/{id}")
+    public ResponseEntity<Blog> updateBlog(@PathVariable("id") Integer id, @RequestBody Blog blog) {
+        System.out.println("Updating blog: " + id);
+        Blog currentBlog = this.blogService.findById(id);
+        if (currentBlog == null) {
+            System.out.println("Current Blog" + id + "not found");
+            return new ResponseEntity<Blog>(HttpStatus.NOT_FOUND);
+        }
+        currentBlog.setCategory(blog.getCategory());
+        currentBlog.setId(blog.getId());
+        currentBlog.setContent(blog.getContent());
+        currentBlog.setDateRelease(blog.getDateRelease());
+        currentBlog.setNameBlog(blog.getNameBlog());
+        currentBlog.setWriter(blog.getWriter());
+        this.blogService.save(currentBlog);
+        return new ResponseEntity<Blog>(currentBlog, HttpStatus.OK);
+    }
+
+    //------------------- Delete a Blog --------------------------------------------------------
+
+    @DeleteMapping("/blog/delete/{id}")
+    public ResponseEntity<Blog> deleteBlog(@PathVariable("id") Integer id) {
+        System.out.println("Delete blog with id: " + id);
+        Blog blog = this.blogService.findById(id);
+        if (blog == null) {
+            System.out.println("Unable to delete with Blog id " + id + "not found");
+            return new ResponseEntity<Blog>(HttpStatus.NOT_FOUND);
+        }
+        this.blogService.deleteById(id);
+        return new ResponseEntity<Blog>(HttpStatus.NO_CONTENT);
+    }
+
     //-------------------Retrieve All Category--------------------------------------------------------
 
     @GetMapping("/category")
@@ -73,5 +117,43 @@ public class BlogController {
             return new ResponseEntity<Category>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<Category>(category, HttpStatus.OK);
+    }
+
+    //-------------------Create a Category--------------------------------------------------------
+    @PostMapping("/category/create")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category, UriComponentsBuilder builder) {
+        this.categoryService.save(category);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(builder.path("/category/create/{id}").buildAndExpand(category.getId()).toUri());
+        return new ResponseEntity<Category>(headers, HttpStatus.CREATED);
+    }
+
+    //------------------- Update a Category --------------------------------------------------------
+    @PutMapping("/category/update/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable("id") Integer id, @RequestBody Category category) {
+        System.out.println("Update category with id: " + id);
+        Category currentCategory = this.categoryService.findById(id);
+        if (currentCategory == null) {
+            System.out.println("Category to update is not found");
+            return new ResponseEntity<Category>(HttpStatus.NOT_FOUND);
+        }
+        currentCategory.setId(category.getId());
+        currentCategory.setName(category.getName());
+        currentCategory.setBlogs(category.getBlogs());
+        this.categoryService.save(currentCategory);
+        return new ResponseEntity<Category>(currentCategory, HttpStatus.OK);
+    }
+
+    //------------------- Delete a Category --------------------------------------------------------
+    @DeleteMapping("/category/delete/{id}")
+    public ResponseEntity<Category> deleteCategory(@PathVariable("id") Integer id) {
+        System.out.println("Delete catagory with id: " + id);
+        Category category = this.categoryService.findById(id);
+        if (category == null) {
+            System.out.println("Category to delete is not found");
+            return new ResponseEntity<Category>(HttpStatus.NOT_FOUND);
+        }
+        this.categoryService.deleteById(id);
+        return new ResponseEntity<Category>(HttpStatus.NO_CONTENT);
     }
 }
