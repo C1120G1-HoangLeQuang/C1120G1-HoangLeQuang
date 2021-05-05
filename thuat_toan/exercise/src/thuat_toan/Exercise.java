a9d05b27eec92445c180a148ca76b8c3ee13e396
Mac <mac@Erwins-MacBook-Air.local>
2021-05-05 08:45:54 +0700

    update

---
+++ thuat_toan/exercise/src/thuat_toan/Exercise.java
@@ -0,0 +1,74 @@
+package thuat_toan;
+
+import java.util.Scanner;
+
+public class Exercise {
+
+//    public static void main(String[] args) {
+//        int checkInteger;
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Please enter a number: ");
+//        checkInteger = input.nextInt();
+//        if (checkInteger > 0) {
+//            System.out.println(checkInteger + " is a Integer");
+//        } else {
+//            System.out.println(checkInteger + " is not a Integer");
+//        }
+//    }
+
+//    public static void main(String[] args) {
+//        int checkInteger;
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Please enter a number: ");
+//        checkInteger = input.nextInt();
+//        switch (checkInteger) {
+//            case 1 :
+//                System.out.println("One");
+//                break;
+//            case 2 :
+//                System.out.println("Two");
+//                break;
+//            case 3 :
+//                System.out.println("Three");
+//                break;
+//            case 4 :
+//                System.out.println("Four");
+//                break;
+//            case 5 :
+//                System.out.println("Five");
+//                break;
+//        }
+//    }
+
+//    public static void main(String[] args) {
+//        float a, b, c;
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Please enter a number: ");
+//        a = input.nextFloat();
+//        b = input.nextFloat();
+//        c = input.nextFloat();
+//        if (a > 0 && b > 0 && c > 0) {
+//            if ((a + b > c) && (a + c > b) && (b + c > a)) {
+//                System.out.println("a, b, c is 3 sides of triangle");
+//            } else {
+//                System.out.println("a, b, c is not 3 sides of triangle");
+//            }
+//        } else {
+//            System.out.println("3 number have to bigger than 0");
+//        }
+//    }
+
+//    public static void main(String[] args) {
+//        String codeStudent;
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter student code: ");
+//        codeStudent = input.nextLine();
+//        if (codeStudent.matches("^B[\\d]{7}$")) {
+//            System.out.println("Match");
+//        } else {
+//            System.out.println("Not Match");
+//        }
+//    }
+
+
+}
---
+++ thuat_toan/exercise/src/thuat_toan/VongLap.java
@@ -0,0 +1,75 @@
+package thuat_toan;
+
+import java.util.Scanner;
+
+public class VongLap {
+
+//    public static void main(String[] args) {
+//        int n;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter number n: ");
+//        n = input.nextInt();
+//        int total = 0;
+//        for (int i = 1; i <= n; i+=2) {
+//            total += i;
+//        }
+//        System.out.println(total);
+//    }
+
+//    public static void main(String[] args) {
+//        int n;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter number n: ");
+//        n = input.nextInt();
+//        for (int i = 1; i <= 20; i++) {
+//            System.out.print(n + "x" + i + "=" + n*i + " || ");
+//        }
+//    }
+
+    //    ========== Print So nguyen to ===========
+//    public static boolean isPrimeNumber(int n) {
+//        // so nguyen n < 2 khong phai la so nguyen to
+//        if (n < 2) {
+//            return false;
+//        }
+//        // check so nguyen to khi n >= 2
+//        int check = (int) Math.sqrt(n);
+//        for (int i = 2; i <= check; i++) {
+//            if (n % i == 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//
+//    public static void main(String[] args) {
+//        int n;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter number n: ");
+//        n = input.nextInt();
+//        for (int i = 0; i <= n; i++) {
+//            if (isPrimeNumber(i)) {
+//                System.out.println(i);
+//            }
+//        }
+//    }
+
+//    ========== tinh fibonaci ===========
+//    public int calculateFi(int n) {
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        return calculateFi(n - 1) + calculateFi(n - 2);
+//    }
+//
+//    public static void main(String[] args) {
+//        int n;
+//        int Fi;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter number n: ");
+//        n = input.nextInt();
+//        VongLap vongLap = new VongLap();
+//        Fi = vongLap.calculateFi(n);
+//        System.out.println("Fi(" + n + ") = " + Fi);
+//    }
+}
