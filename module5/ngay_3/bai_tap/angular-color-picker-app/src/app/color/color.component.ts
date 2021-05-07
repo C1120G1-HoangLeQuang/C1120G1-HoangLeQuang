d84eeb5b05c36f3e9c0b2fb4d7829f1b94e1f1aa
Mac <mac@Erwins-MacBook-Air.local>
2021-05-07 11:09:30 +0700

    update

--- module5/ngay_3/bai_tap/angular-color-picker-app/src/app/color/color.component.ts
+++ module5/ngay_3/bai_tap/angular-color-picker-app/src/app/color/color.component.ts
@@ -6,29 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./color.component.scss']
 })
 export class ColorComponent implements OnInit {
-  // color: string[] = ['#7489ff', '#43d65d', '#d65943', '#d643a7'];
-  selectColor: string;
-
-  colorPicker(color: string) {
-    switch (color) {
-      case '#7489ff' :
-        this.selectColor = color;
-        break;
-      case '#43d65d' :
-        this.selectColor = color;
-        break;
-      case '#d65943' :
-        this.selectColor = color;
-        break;
-      case '#d643a7' :
-        this.selectColor = color;
-        break;
-    }
-  }
+  color: string[] = ['#7489ff', '#43d65d', '#d65943', '#d643a7'];
+  // selectColor: string;
+  //
+  // colorPicker(color: string) {
+  //   switch (color) {
+  //     case '#7489ff' :
+  //       this.selectColor = color;
+  //       break;
+  //     case '#43d65d' :
+  //       this.selectColor = color;
+  //       break;
+  //     case '#d65943' :
+  //       this.selectColor = color;
+  //       break;
+  //     case '#d643a7' :
+  //       this.selectColor = color;
+  //       break;
+  //   }
+  // }
+  tempColor: string;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeColor(value: any) {
+    this.tempColor = value;
+  }
 }
